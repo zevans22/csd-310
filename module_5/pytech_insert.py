fc9388ca1a369f454289966da03b8e0418f69f67
zevans22 <zevans@my365.bellevue.edu>
2022-09-15 16:29:27 -0400

    mongodb_test.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 
+=======
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.i8dgnsj.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client["pytech"]
+collection = db["students"]
+>>>>>>> Stashed changes
