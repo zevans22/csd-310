05455117ee773127a50d1eba7acd17097fea11ab
zevans22 <zevans@my365.bellevue.edu>
2022-09-19 16:53:01 -0400

    Pytech_update_code

--- Module_6/pytech_update.py
+++ Module_6/pytech_update.py
@@ -1,1 +1,7 @@
+my_query = { "last_name": "james" }
 
+new_values = { "$set": { "last_name": "white" } }
+
+x = db.students.updateOne(my_query, new_values)
+
+print (x.modified_count, "The last name of the Student with the ID 1007 has been changed to White.")
