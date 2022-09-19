9add27d0b1d8f827d2c2e90b05b0360acd5f23f9
zevans22 <95583180+zevans22@users.noreply.github.com>
2022-09-19 15:46:33 -0400

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -7,7 +7,7 @@ use('pytech');
 db.students.drop();
 
 db.students.insertMany([
-{"Student_name": "zach", "Student_id": 1007},
-{"Student_name": "bill", "Student_id": 1008},
-{"Student_name": "jane", "Student_id": 1009},
+{"first_name": "zach", "last_name": "james", "Student_id": 1007},
+{"first_name": "bill", "last_name": "brown", "Student_id": 1008},
+{"first_name": "jane", "last_name": "king", "Student_id": 1009},
 ]);
