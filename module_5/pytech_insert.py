333a549a64bb916fe24c07360f49c404d0d4cc15
zevans22 <zevans@my365.bellevue.edu>
2022-09-19 13:46:10 -0400

    pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,7 +1,13 @@
-database = 'pytech'
-collection = 'students_collection'
+const database = 'pytech';
+const collection = 'students';
 
-use (database)
+use(database);
+use('pytech');
 
-db.createCollection(collection)
+db.students.drop();
 
+db.students.insertMany([
+{"Student_name": "zach", "Student_id": 1007},
+{"Student_name": "bill", "Student_id": 1008},
+{"Student_name": "jane", "Student_id": 1009},
+]);
